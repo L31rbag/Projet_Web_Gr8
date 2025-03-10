51bd82aca4974ff8482274600efc0274ebf34bbb
Mathieu <brunotmathieu340@gmail.com>
2025-03-11 00:46:06 +0100

    update page reservation

--- customer/reservation/main.js
+++ customer/reservation/main.js
@@ -3,15 +3,24 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
-      nombre: ""
-    }
-  },
-
-  computed: {
+      nombre: "",
+    };
   },
 
   methods:{
- 
   }
 
 }).mount('#app')
+
+function getDateDuJour() {
+  const ajd = new Date();
+  const annee = ajd.getFullYear();
+  const mois = String(ajd.getMonth() + 1).padStart(2, '0'); // Mois sur 2 chiffres
+  const jour = String(ajd.getDate()).padStart(2, '0'); // Jour sur 2 chiffres
+  return `${annee}-${mois}-${jour}`; // Format AAAA-MM-JJ
+};
+
+const InputDate = document.getElementById('date');
+const date = getDateDuJour();
+InputDate.value = date;
+InputDate.min = date;

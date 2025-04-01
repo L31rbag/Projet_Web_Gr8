6eda240d6f3493101df47a5dc62b85e0fea12327
Maz0k <114288731+Maz0ky@users.noreply.github.com>
2025-04-01 14:32:06 +0200

    ajout réservation

---
+++ user/src/ajout_resa.php
@@ -0,0 +1,35 @@
+<?php
+include_once("../../db/db_connect.php");
+
+$sql = "SELECT texte FROM paragrapheMenu WHERE id = 1";
+$result = $conn->query($sql);
+
+
+if(isset($_POST["nom"])
+&& isset($_POST["telephone"])
+&& isset($_POST["email"])
+&& isset($_POST["nbPersonne"])
+&& isset($_POST["date"])
+&& isset($_POST["service"])
+&& isset($_POST["message"])
+){
+$nom = $_POST["nom"];
+$tel = $_POST["telephone"];
+$mail = $_POST["email"];
+$nbp = $_POST["nbPersonne"];
+$date = $_POST["date"];
+$serv = $_POST["service"];
+$mess = $_POST["message"];
+
+$sql = "INSERT INTO reservation (`nom`, `telephone`, `email`, `nbPersonne`, `date`, `service`, `message`) 
+VALUES ('$nom', '$tel', '$mail', '$nbp', '$date', '$serv', '$mess')";
+if (mysqli_query($conn, $sql)) {
+      echo "Nouveau enregistrement créé avec succès";
+} else {
+      echo "Erreur : " . $sql . "<br>" . mysqli_error($conn);
+}
+}
+mysqli_close($conn);
+header("Location: ../index.html");
+
+?>

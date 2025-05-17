54b0b96d1d1f01363dc275746b414c770c7238ab
Maz0k <114288731+Maz0ky@users.noreply.github.com>
2025-05-17 14:37:09 +0200

    modif script gestion tables

--- gestion_tables/script.js
+++ gestion_tables/script.js
@@ -1,9 +1,3 @@
-var tables = {
-    /* clef = places de la table, 
-    valeur = quantité */
-    2 : 20,
-    4 : 10
-}
 
 function creerMatrice(nbLignes, nbColonnes) {
   const matrice = [];
@@ -20,7 +14,6 @@ function creerMatrice(nbLignes, nbColonnes) {
 
   return matrice;
 }
-
 
 function afficheMatrice(matrice) {
   const nbLignes = matrice.length;
@@ -46,10 +39,75 @@ function afficheMatrice(matrice) {
   }
 }
 
-
-function ajoute(nb_gen){
-
+function nbTables(nb_pers){
+    res = 0;
+    if(nb_pers <= 4){
+      res = 1;
+    }else if(nb_pers <= 6){
+      res = 2;
+    }else{
+      res = 3;
+    }
+    return res;
 }
 
-resto = creerMatrice(12, 12);
-afficheMatrice(resto);
+
+const VIDE = '.'
+const CHAISE = 'x'
+/*
+'.' = vide
+'nb' = table
+'x' = chaise
+*/
+
+function estLibreAutour(matrice, x ,y){
+    const nbLignes = matrice.length;
+    const nbColonnes = matrice[0].length;
+    let g = false, d = false, h = false, b = false;
+
+    //gauche
+    if (x > 0 && matrice[x-1][y] == VIDE){
+      g = true
+    }
+    //droite
+    if (x < nbColonnes-1 && matrice[x+1][y] == VIDE){
+      d = true
+    }
+    //haut
+    if (y > 0 && matrice[x][y-1] == VIDE){
+      h = true
+    }
+    //bas
+    if (y < nbLignes-1 && matrice[x][y+1] == VIDE){
+      b = true
+    }
+    return (g && d && b && h);
+}
+
+function ajouteGroupe(matrice, id, nb_personnes){
+    const nbLignes = matrice.length;
+    const nbColonnes = matrice[0].length;
+    nb_table = nbTables(nb_personnes);
+
+    for (let i = 0; i < nbLignes; i++) {
+        for (let j = 0; j < nbColonnes; j++) {
+        
+        }
+    }
+    return matrice
+}
+
+
+/*  le parse a été fait  en amont */
+function range(data /* id, nom, nb_pers */){
+
+    mat = creerMatrice(12,12);
+    for (let i = 1; i < data.length; i++){
+        let id = data[i]["id"]
+        let nb_pers = data[i]["nb_pers"]
+        mat = ajouteGroupe(mat, id, nb_pers);
+
+
+    }
+    return mat;
+}
